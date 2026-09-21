dd6573b88861abace8e57a5e4bc88326b2d6ab86
twillia2 <travis.williams@nlr.gov>
2026-09-21 09:20:15 -0600

    Updated project points script.

--- tutorial_03_input_builds/3_project_points/make_project_points.py
+++ tutorial_03_input_builds/3_project_points/make_project_points.py
@@ -1,11 +1,15 @@
 """How to build a project points file."""
+from pathlib import Path
+
 from rex import Resource
 
 from rev_tutorial import DATA
 
 
+HOME = Path(__file__).parent
+
 # Path to the HDF5 resource file
-RES_FILE = DATA.joinpath("ri_100_nsrdb_2012.h5")
+RES_FILE = DATA.joinpath("resource/ri_100_nsrdb_2012.h5")
 
 
 def main():
@@ -17,19 +21,18 @@ def main():
     # The "gid" column is the original index and tell reV where to run
     meta.loc[:, "gid"] = meta.index
 
-    # Now you can subset, as long as the gid refers to the original index
-    meta = meta[meta["population"] < 10_000]
-
     # The config column tell reV which SAM config to run
     # Here we are assuming a single 'default' config will be run every where
     meta.loc[:, "config"] = "default"
+
+    # Now you can subset, as long as the gid refers to the original index
+    meta = meta[meta["population"] < 10_000]
 
     # All reV needs is config and gid, but you can include other fields
     project_points = meta[["latitude", "longitude", "gid", "config"]]
 
     # Save this to a project points file
-    fname = "project_points.csv"
-    dst = HOME.joinpath(f"tutorial_3_input_builds/make_project_points/{fname}")
+    dst = HOME.joinpath("project_points.csv")
     project_points.to_csv(dst, index=False)
 
 
